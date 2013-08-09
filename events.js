b97a65c0c52b5b8f32f3dcdb52bde02b32584eff
py <pengyang238@gmail.com>
2013-08-09 16:36:24 +0800

    add files

---
+++ events.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ADD_SERVERS: '__add_servers__',
+  REMOVE_SERVERS: '__remove_servers__',
+  REPLACE_SERVERS: '__replace_servers__',
+  NEW_REQ: '__new_request__',
+  NEW_RESP: '__new_response__'
+};
